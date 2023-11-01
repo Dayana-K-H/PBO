d168fdabd4b6b5c9adbc8bfa0af796488c1f76ad
Dayana K Harahap <144566392+Dayana-K-H@users.noreply.github.com>
2023-11-01 20:29:08 +0700

    Update Karyawan.java
    Perubahan pada variabel
    Penambahan constructor kosong
    Penambahan method tampilkan data

--- Karyawan/src/Gaji/Karyawan.java
+++ Karyawan/src/Gaji/Karyawan.java
@@ -1,14 +1,19 @@
 package Gaji;
 
+import java.util.Scanner;
+
 public class Karyawan {
     protected String nama;
     protected String nip;
-    protected int gajiPokok;
+    protected int gajiPokok = 2000000;
+    
+    public Karyawan(){
 
+    }
+    
     public Karyawan(String nama, String nip){
         this.nama = nama;
         this.nip = nip;
-        this.gajiPokok = 2000000;
     }
 
     public void setNama(String nama){
@@ -39,8 +44,24 @@ public class Karyawan {
         return gajiPokok;
     }
 
-    public void input() {
+    public void tampilkanData(){
+        System.out.println("---- Data Karyawan ----");
+        System.out.println("Nama  : " + this.nama);
+        System.out.println("Nip   : " + this.nip);
+        System.out.println("Gaji pokok  : " + getGajiPokok());
+        System.out.println();
 
+    }
+    public void input() {
+        Scanner inputScanner = new Scanner(System.in);
+
+        System.out.println("---- Data Karyawan ----");
+        System.out.print("Nama  : ");
+        this.nama = inputScanner.next();
+        System.out.print("Nip   : ");
+        this.nip = inputScanner.next();
+        System.out.println("Gaji pokok  : " + getGajiPokok());
+        System.out.println();
     }
 
 }
